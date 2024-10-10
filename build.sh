06b0e8c7a0079a02c872ec73e5a41bc44e8d46d3
Fakhri Mouad <moad_fakhri_1999@outlook.com>
2024-10-10 14:23:58 +0100

    add build script for linux

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+app_name="mplayer"
+
+code="$PWD"
+common_opts="-show-timings -collection:src=$code/code -microarch:native -warnings-as-errors -vet"
+
+generate_debug_info="1"
+release_mode="0"
+
+if [[ "$release_mode" == "0" ]]; then
+  common_opts="$common_opts -o:none"
+else
+  common_opts="$common_opts -o:speed"
+fi
+
+if [[ "$generate_debug_info" == "1" ]]; then
+  common_opts="$common_opts -debug"
+fi
+
+mkdir -p $code/bld
+cd $code/bld > /dev/null
+
+odin build $code/code -out:${app_name}.exe $common_opts
+
+cd $code > /dev/null
